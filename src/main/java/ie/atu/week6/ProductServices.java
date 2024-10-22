fab50c7ec95ef1602ebc32f733b154099dbed580
G00419446@atu.ie <jseeruthen26@outlook.coom>
2024-10-22 11:09:51 +0100

    Made 3 classes and populated them

---
+++ src/main/java/ie/atu/week6/ProductServices.java
@@ -0,0 +1,22 @@
+package ie.atu.week6;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductServices {
+
+    private List<Product> myList = new ArrayList<>();
+    public List<Product> addProduct(Product product)
+    {
+        //do buisness stuff like retrieving details from db or generating files pdfs
+        myList.add(product);
+        return myList;
+    }
+}
